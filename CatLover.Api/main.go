ed67d6d7a61b066d3ca484f321032d385b25fb97
Giorgos Michalagas <gmichalagas@outlook.com>
2022-01-23 19:43:31 +0200

    Minor updates.

--- CatLover.Api/main.go
+++ CatLover.Api/main.go
@@ -19,14 +19,18 @@ func homePage(c *gin.Context) {
 func handleRequests() {
 	router := gin.Default()
 	router.GET("/", homePage)
+
+	//Cats route
 	router.GET("/cats", controllers.GetAllCats)
 	router.GET("/catsfavorites", controllers.GetAllCatsFavorites)
 	router.GET("/cats/:id", controllers.GetCatById)
 	router.GET("/catsbreed/:id", controllers.GetCatsByBreedId)
 	router.PATCH("/cats/:id", controllers.UpdateCat)
 
+	//Breeds route
 	router.GET("/breeds", controllers.GetAllBreeds)
 	router.GET("/breeds/:id", controllers.GetBreedById)
+
 	router.NoRoute(func(c *gin.Context) {
 		c.IndentedJSON(http.StatusNotFound, services.ReturnResponse("404 Not Found", 404, "Page not found.", nil))
 	})
