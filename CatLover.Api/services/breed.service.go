23e26c5f67c07cea2892a91c3226283eb86926cd
Giorgos Michalagas <gmichalagas@outlook.com>
2022-01-23 20:29:49 +0200

    Interface added.

--- CatLover.Api/services/breed.service.go
+++ CatLover.Api/services/breed.service.go
@@ -10,13 +10,19 @@ import (
 	"catlover.api/models"
 )
 
+type BreedService interface {
+	GetAllBreeds() []models.Breed
+	GetOneBreed(id string) (models.Breed, int32)
+	OpenBreedJsonFile() []models.Breed
+}
+
 func GetAllBreeds() []models.Breed {
-	var breeds = OpenBreedJsonFile()
+	var breeds = OpenBreedJsonFile() //Better use of SQL DB
 	return breeds
 }
 
 func GetOneBreed(id string) (models.Breed, int32) {
-	var breeds = OpenBreedJsonFile()
+	var breeds = OpenBreedJsonFile() //Better use of SQL DB
 	for index := range breeds {
 		if breeds[index].Id == id {
 			return breeds[index], 1
