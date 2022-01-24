b3deb7a27ab7ebbb3af8ad93211b68b4c6502fa5
Giorgos Michalagas <gmichalagas@outlook.com>
2022-01-24 16:21:19 +0200

    React Initial setup.

---
+++ catlover.client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="alert alert-primary" role="alert">
+      A simple primary alert—check it out!
+    </div>
+);
+}
+
+export default App;
