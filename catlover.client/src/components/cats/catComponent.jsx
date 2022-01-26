c67042c0ac0d14b5bd197f670299a23e94e357af
Giorgos Michalagas <gmichalagas@outlook.com>
2022-01-27 00:01:46 +0200

    CatComponent updated with CatModalComponent

---
+++ catlover.client/src/components/cats/catComponent.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import CatModalComponent from "./catModalComponent";
+
+class CatComponent extends Component {
+  state = {};
+  test = {};
+
+  handleShow = () => {
+    this.setState({
+      show: true,
+      title: "Group People",
+      body: "Hi, find my group details",
+    });
+  };
+
+  handleClose = (fromModal) => {
+    alert(fromModal.msg);
+
+    this.setState({
+      show: false,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button
+          type="button"
+          className="btn btn-light"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+          onClick={this.handleShow}
+        >
+          <img
+            src={this.props.photoUrl}
+            className="card-img-top img-fluid img-rounded"
+            alt={this.props.photoUrl}
+          />
+        </button>
+        <CatModalComponent
+          show={this.state.show}
+          title={this.state.title}
+          body={this.state.body}
+          data={this.props}
+          onClick={this.handleClose}
+          onHide={this.handleClose}
+        />
+      </div>
+    );
+  }
+
+  imageClicked(text) {
+    this.test = text;
+    console.log("cat " + this.test);
+  }
+}
+
+export default CatComponent;
