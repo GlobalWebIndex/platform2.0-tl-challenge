ecfcaf3a6d60f6d6322e5a8c77106dc64f426304
Giorgos Michalagas <gmichalagas@outlook.com>
2022-01-27 18:01:42 +0200

    react-router-dom installed. Router & routes added.

--- catlover.client/src/App.js
+++ catlover.client/src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import CatsComponent from './components/cats/catsComponent'
+import BreedsComponent from './components/breeds/breedsComponent'
+import HomeComponent from './components/home/homeComponent'
+import PageNotFoundComponent from './components/pageNotFound/pageNotFoundComponent';
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
 function App() {
-  return ( <
-    CatsComponent / >
-  );
+  return (
+    <Router>
+        <Routes>
+          <Route exact path="/" element={<HomeComponent/>}/>
+          <Route exact path="/cats" element={<CatsComponent/>}/>
+          <Route exact path="/breeds" element={<BreedsComponent/>}/>
+          <Route path="*" element={<PageNotFoundComponent/>}/>
+        </Routes>
+    </Router>
+    );
 }
 
 export default App;
