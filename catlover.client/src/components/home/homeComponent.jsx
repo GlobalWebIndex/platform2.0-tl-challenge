9b8590a382f4e96a7aa8daaf37c59871113eb352
Giorgos Michalagas <gmichalagas@outlook.com>
2022-01-27 18:02:26 +0200

    Basic Links added.

--- catlover.client/src/components/home/homeComponent.jsx
+++ catlover.client/src/components/home/homeComponent.jsx
@@ -4,46 +4,46 @@ class HomeComponent extends Component {
   state = {};
   render() {
     return (
-      <div class="row row-cols-1 row-cols-md-3 g-4 m-2">
-        <div class="col">
-          <div class="card">
-            <a href="/cats" class="stretched-link"></a>
-            <div class="card-header">
-              <h5 class="card-title">Cats List</h5>
+      <div className="row row-cols-1 row-cols-md-3 g-4 m-2">
+        <div className="col">
+          <div className="card">
+            <a href="/cats" className="stretched-link"></a>
+            <div className="card-header">
+              <h5 className="card-title">Cats List</h5>
             </div>
-            <div class="card-body">
-              <p class="card-text">Go to cats photos.</p>
+            <div className="card-body">
+              <p className="card-text">Go to cats photos.</p>
             </div>
-            <div class="card-footer">
-              <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card">
-            <a href="/breeds" class="stretched-link"></a>
-            <div class="card-header">
-              <h5 class="card-title">Breeds List</h5>
-            </div>
-            <div class="card-body">
-              <p class="card-text">Go to cats breeds.</p>
-            </div>
-            <div class="card-footer">
-              <small class="text-muted">Last updated 3 mins ago</small>
+            <div className="card-footer">
+              <small className="text-muted">Last updated 3 mins ago</small>
             </div>
           </div>
         </div>
-        <div class="col">
-          <div class="card">
-            <a href="/breeds" class="stretched-link"></a>
-            <div class="card-header">
-              <h5 class="card-title">Cats Favorite List</h5>
+        <div className="col">
+          <div className="card">
+            <a href="/breeds" className="stretched-link"></a>
+            <div className="card-header">
+              <h5 className="card-title">Breeds List</h5>
             </div>
-            <div class="card-body">
-              <p class="card-text">Go to favorite cats.</p>
+            <div className="card-body">
+              <p className="card-text">Go to cats breeds.</p>
             </div>
-            <div class="card-footer">
-              <small class="text-muted">Last updated 3 mins ago</small>
+            <div className="card-footer">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <a href="/breeds" className="stretched-link"></a>
+            <div className="card-header">
+              <h5 className="card-title">Cats Favorite List</h5>
+            </div>
+            <div className="card-body">
+              <p className="card-text">Go to favorite cats.</p>
+            </div>
+            <div className="card-footer">
+              <small className="text-muted">Last updated 3 mins ago</small>
             </div>
           </div>
         </div>
