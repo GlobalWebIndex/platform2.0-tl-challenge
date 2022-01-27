1ead012af09fb1a91cda4a36e6423af2cc6ed72a
Giorgos Michalagas <gmichalagas@outlook.com>
2022-01-28 01:13:00 +0200

    Rout CatsFavoriteComponent added.

--- catlover.client/src/App.js
+++ catlover.client/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import CatsComponent from './components/cats/catsComponent'
 import BreedsComponent from './components/breeds/breedsComponent'
+import CatsFavoriteComponent from './components/favorites/catsFavoritesComponent';
 import HomeComponent from './components/home/homeComponent'
 import PageNotFoundComponent from './components/pageNotFound/pageNotFoundComponent';
 import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
@@ -13,6 +14,7 @@ function App() {
           <Route exact path="/" element={<HomeComponent/>}/>
           <Route exact path="/cats" element={<CatsComponent/>}/>
           <Route exact path="/breeds" element={<BreedsComponent/>}/>
+          <Route exact path="/favorites" element={<CatsFavoriteComponent/>}/>
           <Route path="*" element={<PageNotFoundComponent/>}/>
         </Routes>
     </Router>
