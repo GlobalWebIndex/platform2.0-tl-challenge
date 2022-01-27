5f1ecdba57b18aa90109f1a6277349cdb87a1824
Giorgos Michalagas <gmichalagas@outlook.com>
2022-01-27 11:47:59 +0200

    PageNotFound Component created

---
+++ catlover.client/src/components/pageNotFound/pageNotFoundComponent.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class PageNotFoundComponent extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <img
+          src="/PageNotFound.jpg"
+          className="card-img-top img-fluid img-rounded"
+          alt="Page Not Found"
+        />
+      </div>
+    );
+  }
+}
+
+export default PageNotFoundComponent;
