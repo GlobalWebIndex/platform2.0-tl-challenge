36ae33828c6868540a2d08a734fdece6502649fa
Giorgos Michalagas <gmichalagas@outlook.com>
2022-02-01 20:04:30 +0200

    Updated.

--- catlover.client/src/services/requests.service.jsx
+++ catlover.client/src/services/requests.service.jsx
@@ -1,8 +1,14 @@
-const baseURL = "http://localhost:10000/";
+const API_URL = "http://localhost:10000/";
 
 const RequestsService = {
   GetAllData: function (model) {
-    fetch(baseURL + model)
+    fetch(API_URL + model, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "GET",
+    })
       .then((result) => result.json())
       .then(
         (result) => {
@@ -14,9 +20,48 @@ const RequestsService = {
       );
   },
 
-  GetOneDataById: function (model, id) {},
+  GetOneDataById: function (model, id) {
+    fetch(API_URL + model + "/" + id, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "GET",
+    })
+      .then((result) => result.json())
+      .then(
+        (result) => {
+          return [result, null];
+        },
+        (error) => {
+          return [null, error];
+        }
+      );
+  },
 
-  UpdateDataById: function (model, id) {},
+  UpdateDataById: function (model, id) {
+    fetch(API_URL + model + "/" + id, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "PATCH",
+      body: JSON.stringify({
+        breedId: this.props.breedId,
+        photoUrl: this.props.photoUrl,
+        isFavorite: isFavorite,
+      }),
+    })
+      .then((result) => result.json())
+      .then(
+        (result) => {
+          return [result, null];
+        },
+        (error) => {
+          return [null, error];
+        }
+      );
+  },
 };
 
 export default RequestsService;
